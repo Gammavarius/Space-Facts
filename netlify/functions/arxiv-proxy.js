ff3d04584f0a69d9ef4e51ddd1b2350314e33eb7
Gammavarius <blind.gammavarius@gmail.com>
2026-04-02 11:48:33 +0300

    fix: switch arXiv proxy to axios, rename env var

--- netlify/functions/arxiv-proxy.js
+++ netlify/functions/arxiv-proxy.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import axios from "axios";
 
 const BASE_URL = 'https://export.arxiv.org/api/query'
 const keysArray = ['exoplanet', 'supernova', 'black+hole', 'galaxy', 'nebula', 'pulsar', 'quasar', 'dark+matter', 'gravitational+waves'];
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
     const url = `${BASE_URL}?search_query=all:${encodeURIComponent(key)}&start=0&max_results=1`;
 
     try {
-    const response = await fetch(url);
+    const response = await axios.get(url);
     const data = await response.text();
     
     return {
